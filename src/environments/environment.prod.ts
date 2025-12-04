f9fb1854b58c20da1dfafc3ead9260b6e9ca91e9
224rifche <cherif2862@gmail.com>
2025-12-04 14:31:56 +0000

    refactor(github-service): remove token authentication and update service

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// src/environments/environment.prod.ts
+
+export const environment = {
+  production: true,
+  
+    token: 'GIT_TOKEN', // Le token sera défini dans les variables d'environnement du serveur
+ 
+
+  
+};
